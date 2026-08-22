5d847a3b3783c4024d3ceae99cab94eab61beed4
Joao Berga <iceberga91@gmail.com>
2026-08-22 23:40:21 +0100

    https://github.com/iceberga91-pixel/TraffigLight.git

---
+++ app.py
@@ -0,0 +1,13 @@
+time = int(input("How many seconds have passed?"))
+
+while time > 60:
+    time = time - 60
+
+if time < 40:
+    print("RED")
+
+elif time < 50:
+    print("YELLOW")
+
+elif time <= 60:
+    print("GREEN")
